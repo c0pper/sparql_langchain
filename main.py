1a694aea7c38eea47fd73633bc050927d1de23d9
Simone Martin Marotta <smarotta@expert.ai>
2024-02-14 10:02:24 +0100

    update

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from langchain.chains import GraphSparqlQAChain
 from langchain.graphs import RdfGraph
 from dotenv import load_dotenv
 import os
+from rdflib import Graph
+
+# g = Graph()
+# g.parse("http://raw.githubusercontent.com/c0pper/sparql_langchain/main/card.rdf", format='xml')
+# print(len(g))
+# import pprint
+# for stmt in g:
+#     pprint.pprint(stmt)
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 graph = RdfGraph(
-    source_file="https://rdftest420.000webhostapp.com/card.rdf",
-    standard="rdf",
-    local_copy="test.ttl",
+    source_file="uni.rdf",
+    standard="rdfs",
+    serialization='xml',
+    local_copy="test.rdf",
 )
 
 graph.load_schema()
@@ -19,5 +28,6 @@ print(graph.get_schema)
 chain = GraphSparqlQAChain.from_llm(
     ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY), graph=graph, verbose=True
 )
-
-print(chain.run("What is Tim Berners-Lee's work homepage?"))
+q = "Which researchers employed at Università di Napoli collaborate with Università di Messina?"
+print(f"Q: {q}")
+print(chain.run(q))
